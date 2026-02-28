3625aadc9f9985e8855532089f161dd6c3e71c39
Garren Miller <garrenkmiller@gmail.com>
2026-02-27 22:54:28 -0700

    reading some data

--- src/main.rs
+++ src/main.rs
@@ -6,43 +6,33 @@ use waverave_hackrf::open_hackrf;
 mod zadoff_chu;
 
 
-// #[tokio::main]
-// async fn main() -> Result<()> {
-//     let hackrf = open_hackrf()?;
-//
-//     // Configure for your tower
-//     hackrf.set_sample_rate(30_000_000.0).await?;
-//     hackrf.set_amp_enable(false).await?; 
-//     hackrf.set_lna_gain(24).await?;
-//     hackrf.set_vga_gain(30).await?;
-//     hackrf.set_freq(2_130_000_000).await?; // Tower is at 2127.5 MHz, but we want to have a bit of
-//                                            // offset for better data
-//
-//     println!("[*] Capturing to 'lte_capture.raw'...");
-//     let mut file = File::create("lte_capture.c8")?;
-//
-//     // Start Receiving
-//     let mut hackrf_rx = hackrf.start_rx(131072).await.map_err(|e| e.err)?;
-//     for _ in 0..128 { hackrf_rx.submit(); }
-//
-//     // Capture for about 2 seconds (~80MB of data)
-//     for i in 0..300 {
-//         let buf = hackrf_rx.next_complete().await?;
-//
-//         // The HackRF returns interleaved I and Q as i8.
-//         // We write the raw byte buffer directly to the file.
-//         file.write_all(buf.bytes())?;
-//
-//         hackrf_rx.submit();
-//         if i % 50 == 0 { println!("  > Wrote chunk {}...", i); }
-//     }
-//
-//     hackrf_rx.stop().await?;
-//     println!("[+] Capture complete! Use 'Inspectrum' to view.");
-//     Ok(())
-// }
+#[tokio::main]
+async fn main() -> Result<()> {
+    let mut hackrf = open_hackrf()?;
+    hackrf.set_sample_rate(15_360_000.0).await?;
+    hackrf.set_lna_gain(24).await?;
+    hackrf.set_vga_gain(16).await?;
+    hackrf.set_freq(754_000_000).await?;
 
+    let mut file = File::create("lte_capture.c8")?;
+    let mut rx = hackrf.start_rx(131072).await.map_err(|e| e.err)?;
+    
+    for _ in 0..128 { rx.submit(); }
 
-fn main() {
-    zadoff_chu::get_zc_sequence();
+    // 23.04M samples/sec * 0.1 sec * 2 bytes/sample = 4,608,000 bytes total
+    // With 131,072 byte chunks, 35 iterations is ~0.1 seconds.
+    for _ in 0..24 {
+        let buf = rx.next_complete().await?;
+        file.write_all(buf.bytes())?;
+        rx.submit();
+    }
+
+    rx.stop().await?;
+    Ok(())
 }
+
+
+// fn main() {
+//     zadoff_chu::get_zc_sequence();
+//     zadoff_chu::get_correlation_values();
+// }
