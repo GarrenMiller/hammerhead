7f5c3c344fb47ff3f252ff2cb2463be1243c4bc5
GarrenMiller <garrenkmiller@gmail.com>
2026-03-01 00:07:20 -0700

    detecting PSS spike, I think

--- src/lte/c8.rs
+++ src/lte/c8.rs
@@ -11,6 +11,8 @@ fn read_file_in_byte_chunks(path: std::path::PathBuf) -> Result<(), Box<dyn std:
     let file = File::open(path)?;
     let mut reader = BufReader::with_capacity(window_size, file);
     let zc_sequence = zadoff_chu::get_zc_sequence(25);
+    let mut largest = 0;
+    let mut counter = 0;
 
     loop {
         let buffer = reader.fill_buf()?;
@@ -21,16 +23,22 @@ fn read_file_in_byte_chunks(path: std::path::PathBuf) -> Result<(), Box<dyn std:
         }
 
         let input: Vec<i8> = buffer.iter().map(|&x| x as i8).collect();
-        get_chunk_correlation(&zc_sequence, input);
+        let correlation = get_chunk_correlation(&zc_sequence, input);
+        if correlation.abs() > largest {
+            largest = correlation.abs();
+            counter += 1;
+        }
         
-
         reader.consume(2);
     }
+
+    println!("The largest value is: {largest} at index {counter}");
+
 
     Ok(())
 }
 
-fn get_chunk_correlation(zc_sequence: &Vec<i8>, input_sequence: Vec<i8>) {
+fn get_chunk_correlation(zc_sequence: &Vec<i8>, input_sequence: Vec<i8>) -> i32 {
     // The signal cross-correlation sequence rxy[m] of discrete-time signals x[n] and y[n] is the sum from n = 0 to n = N_1
     // of x[n] * y[n - m] where N_1 is the index of the last element in the sequence, and m is the
     // time shift (lag) between signals.
@@ -41,5 +49,5 @@ fn get_chunk_correlation(zc_sequence: &Vec<i8>, input_sequence: Vec<i8>) {
         .map(|(z, i)| ((i[0] as i32 + i[1] as i32) * (i[0] as i32 - i[1] as i32)))
         .sum();
 
-    println!("Correlation is: {}", result)
+    result
 }
